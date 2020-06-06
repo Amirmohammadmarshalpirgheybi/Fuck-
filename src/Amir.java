6bb46c0174d5c7821149f1b52ed067c3b36b4d5e
amirmohammad marshalpiegheybi <228alimarshal@gmail.com>
2020-06-06 11:02:09 +0430

    go to hello

--- src/Amir.java
+++ src/Amir.java
@@ -1,5 +1,8 @@
 public class Amir {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("I am tired");
+        System.out.println("go to hell");
+
     }
 }
