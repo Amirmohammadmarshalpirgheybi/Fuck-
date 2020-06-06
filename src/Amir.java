1e5c7933ea58c3fda84a40829b601089719a60e2
amirmohammad marshalpiegheybi <228alimarshal@gmail.com>
2020-06-06 11:04:13 +0430

    this is me

--- src/Amir.java
+++ src/Amir.java
@@ -3,6 +3,8 @@ public class Amir {
         System.out.println("Hello world");
         System.out.println("I am tired");
         System.out.println("go to hell");
+        String[] ali=new String[6];
+        ali[5]="GO to hell";
 
     }
 }
